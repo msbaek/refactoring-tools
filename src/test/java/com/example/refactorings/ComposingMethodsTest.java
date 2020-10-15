673efd8729dc07e6793433ed9f45fab1c628d8f0
Myeongseok Baek <ms.baek@sk.com>
2020-10-15 18:09:47 +0900

    before Replace Method with Method Object

--- src/test/java/com/example/refactorings/ComposingMethodsTest.java
+++ src/test/java/com/example/refactorings/ComposingMethodsTest.java
@@ -106,4 +106,20 @@ public class ComposingMethodsTest {
 
         return result;
     }
+
+    class Account {
+        private int delta() {
+            throw new UnsupportedOperationException();
+        }
+
+        int gamma(int inputVal, int quantity, int yearToDate) {
+            int importantValue1 = (inputVal * quantity) + delta();
+            int importantValue2 = (inputVal * yearToDate) + 100;
+            if ((yearToDate - importantValue1) > 100)
+                importantValue2 -= 20;
+            int importantValue3 = importantValue2 * 7;
+            // and so on.
+            return importantValue3 - 2 * importantValue1;
+        }
+    }
 }
