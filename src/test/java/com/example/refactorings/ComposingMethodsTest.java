f3804893a2d133fc4c9b69eb429eb7e23232f7c2
Myeongseok Baek <ms.baek@sk.com>
2020-10-15 12:53:14 +0900

    substitute expression with temp var.

--- src/test/java/com/example/refactorings/ComposingMethodsTest.java
+++ src/test/java/com/example/refactorings/ComposingMethodsTest.java
@@ -68,6 +68,6 @@ public class ComposingMethodsTest {
         final int basePrice = quantity * itemPrice;
         return basePrice -
                 Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+                Math.min(basePrice * 0.1, 100.0);
     }
 }
