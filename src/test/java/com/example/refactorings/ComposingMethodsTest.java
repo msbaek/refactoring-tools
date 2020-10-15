2ddd3af1174b628c594d60b5b147045bf86a6988
Myeongseok Baek <ms.baek@sk.com>
2020-10-15 15:17:50 +0900

    remove assignments to parameter

--- src/test/java/com/example/refactorings/ComposingMethodsTest.java
+++ src/test/java/com/example/refactorings/ComposingMethodsTest.java
@@ -98,9 +98,12 @@ public class ComposingMethodsTest {
     }
 
     int discount(int inputVal, int quantity, int yearToDate) {
-        if (inputVal > 50) inputVal -= 2;
-        if (quantity > 100) inputVal -= 1;
-        if (yearToDate > 10000) inputVal -= 4;
-        return inputVal;
+        int result = inputVal;
+
+        if (inputVal > 50) result -= 2;
+        if (quantity > 100) result -= 1;
+        if (yearToDate > 10000) result -= 4;
+
+        return result;
     }
 }
