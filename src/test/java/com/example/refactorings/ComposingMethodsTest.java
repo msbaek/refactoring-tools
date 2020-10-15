4b69fceeab79a7fbe90a59d24881dfa4ad571f56
Myeongseok Baek <ms.baek@sk.com>
2020-10-15 15:15:32 +0900

    before remove assignments to parameter

--- src/test/java/com/example/refactorings/ComposingMethodsTest.java
+++ src/test/java/com/example/refactorings/ComposingMethodsTest.java
@@ -96,4 +96,11 @@ public class ComposingMethodsTest {
         }
         return result;
     }
+
+    int discount(int inputVal, int quantity, int yearToDate) {
+        if (inputVal > 50) inputVal -= 2;
+        if (quantity > 100) inputVal -= 1;
+        if (yearToDate > 10000) inputVal -= 4;
+        return inputVal;
+    }
 }
