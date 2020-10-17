5e379ce6fdb70cbb47ed0c7360384457e97a6842
Myeongseok Baek <ms.baek@sk.com>
2020-10-17 12:10:43 +0900

    move method

--- src/test/java/com/example/refactorings/MovingFeaturesTest.java
+++ src/test/java/com/example/refactorings/MovingFeaturesTest.java
@@ -4,24 +4,25 @@ class AccountType {
     public boolean isPremium() {
         throw new UnsupportedOperationException();
     }
-}
 
-class Account {
-    double overdraftCharge() {
-        if (_type.isPremium()) {
+    double overdraftCharge(Account account) {
+        if (isPremium()) {
             double result = 10;
-            if (_daysOverdrawn > 7)
-                result += (_daysOverdrawn - 7) * 0.85;
+            if (account._daysOverdrawn > 7)
+                result += (account._daysOverdrawn - 7) * 0.85;
             return result;
         }
         else
-            return _daysOverdrawn * 1.75;
+            return account._daysOverdrawn * 1.75;
     }
+}
+
+class Account {
 
     double bankCharge() {
         double result = 4.5;
         if (_daysOverdrawn > 0)
-            result += overdraftCharge();
+            result += _type.overdraftCharge(this);
         return result;
     }
 
