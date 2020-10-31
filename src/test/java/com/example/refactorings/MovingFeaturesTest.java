552a9489939cdb5ba097a963644076e508d31462
Myeongseok Baek <ms.baek@sk.com>
2020-11-01 01:45:58 +0900

    대리 객체를 필요로 하는 클라이언트가 더 이상 없으면 서버에서 대리 객체에 대한 접근자를 제거

--- src/test/java/com/example/refactorings/MovingFeaturesTest.java
+++ src/test/java/com/example/refactorings/MovingFeaturesTest.java
@@ -115,10 +115,6 @@ class TelephoneNumber {
 class Person {
     Department _department;
 
-    public Department getDepartment() {
-        return _department;
-    }
-
     public void setDepartment(Department arg) {
         _department = arg;
     }
