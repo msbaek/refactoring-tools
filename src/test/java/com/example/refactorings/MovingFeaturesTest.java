02b62084d60dd58e178e5a07aeb71ce638e8b1cf
Myeongseok Baek <ms.baek@sk.com>
2020-11-10 21:20:52 +0900

    extract method - 기능을 필요로하는 클라이언트 클래스에 메소드를 추가 - 서버 클래스의 인스턴스를 첫번째 인자로 전달

--- src/test/java/com/example/refactorings/MovingFeaturesTest.java
+++ src/test/java/com/example/refactorings/MovingFeaturesTest.java
@@ -63,6 +63,10 @@ public class MovingFeaturesTest {
     void introduceForeignMethod() {
         Date previousEnd = new Date();
 
+        nextDay(previousEnd);
+    }
+
+    private static void nextDay(Date previousEnd) {
         Date newStart = new Date(previousEnd.getYear(),
                 previousEnd.getMonth(), previousEnd.getDate() + 1);
     }
