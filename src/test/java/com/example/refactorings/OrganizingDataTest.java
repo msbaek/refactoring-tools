9630fac799fb5c10c7a06876ca57612bd3e9003e
Myeongseok Baek <ms.baek@sk.com>
2020-12-15 17:46:55 +0900

    before Replace Data Value with Object

--- src/test/java/com/example/refactorings/OrganizingDataTest.java
+++ src/test/java/com/example/refactorings/OrganizingDataTest.java
@@ -10,46 +10,15 @@ import java.util.Objects;
 import java.util.Observable;
 import java.util.Observer;
 
-class Customer {
+class Order {
     private final String name;
 
-    private Customer(String name) {
+    public Order(String name) {
         this.name = name;
-    }
-
-    public static Customer create(String name) {
-        return new Customer(name);
     }
 
     public String getName() {
         return name;
-    }
-}
-
-class Order {
-    public Order(String customerName) {
-        customer = Customer.create(customerName);
-    }
-
-    public void setCustomer(String customerName) {
-        customer = Customer.create(customerName);
-    }
-
-    public String getCustomerName() {
-        return customer.getName();
-    }
-
-    private Customer customer;
-}
-
-class ChangeValueToReferenceClient {
-    private static int numberOfOrdersFor(List<Order> orders, String customer) {
-        int result = 0;
-        for (Order each : orders) {
-            if (each.getCustomerName()
-                    .equals(customer)) result++;
-        }
-        return result;
     }
 }
 
