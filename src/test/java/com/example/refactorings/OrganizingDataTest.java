e840ea6decb7baf2008f22c8ddf0b1788fd00730
Myeongseok Baek <ms.baek@sk.com>
2021-01-05 10:04:54 +0900

    before Replace Type Code with Subclasses

--- src/test/java/com/example/refactorings/OrganizingDataTest.java
+++ src/test/java/com/example/refactorings/OrganizingDataTest.java
@@ -364,4 +364,14 @@ public class OrganizingDataTest {
         assertThat(advancedCourceCount).isEqualTo(1);
     }
 
+    class Employee {
+        private int type;
+        static final int ENGINEER = 0;
+        static final int SALESMAN = 1;
+        static final int MANAGER = 2;
+
+        Employee (int type) {
+            this.type = type;
+        }
+    }
 }
