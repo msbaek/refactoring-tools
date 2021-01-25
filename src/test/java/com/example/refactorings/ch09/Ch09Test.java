3f38d0de3129d3ee519e05060a2e2d5bb5d15e4d
Myeongseok Baek <ms.baek@sk.com>
2021-01-25 19:17:16 +0900

    before Introduce Assertion

--- src/test/java/com/example/refactorings/ch09/Ch09Test.java
+++ src/test/java/com/example/refactorings/ch09/Ch09Test.java
@@ -135,4 +135,26 @@ public class Ch09Test {
 
         int weekDelinquent = customer.getWeeksDelinquentInLastYear();
     }
+
+    class Project {
+        public double getMemberExpenseLimit() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    class OurEmployee {
+        private static final double NULL_EXPENSE = -1.0;
+        private double _expenseLimit = NULL_EXPENSE;
+        private Project _primaryProject;
+
+        double getExpenseLimit() {
+            return (_expenseLimit != NULL_EXPENSE) ?
+                    _expenseLimit :
+                    _primaryProject.getMemberExpenseLimit();
+        }
+
+        boolean withinLimit(double expenseAmount) {
+            return (expenseAmount <= getExpenseLimit());
+        }
+    }
 }
