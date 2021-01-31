e94c48a4618dbedb4cca812231f30cd034344e2f
Myeongseok Baek <ms.baek@sk.com>
2021-01-31 16:45:38 +0900

    before Separate Query from Modifier

---
+++ src/test/java/com/example/refactorings/ch10/Ch10Test.java
@@ -0,0 +1,30 @@
+package com.example.refactorings.ch10;
+
+public class Ch10Test {
+    private void sendAlert() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void someLaterCode(String found) {
+        throw new UnsupportedOperationException();
+    }
+
+    String foundMiscreant(String[] people){
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].equals ("Don")){
+                sendAlert();
+                return "Don";
+            }
+            if (people[i].equals ("John")){
+                sendAlert();
+                return "John";
+            }
+        }
+        return "";
+    }
+
+    void checkSecurity(String[] people) {
+        String found = foundMiscreant(people);
+        someLaterCode(found);
+    }
+}
