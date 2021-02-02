61a8d0ee55dffc772397e9ef97d49bdcf8fae484
Myeongseok Baek <ms.baek@sk.com>
2021-02-02 11:55:39 +0900

    before Replace Parameter with Method

--- src/test/java/com/example/refactorings/ch10/Ch10Test.java
+++ src/test/java/com/example/refactorings/ch10/Ch10Test.java
@@ -3,6 +3,9 @@ package com.example.refactorings.ch10;
 import org.junit.jupiter.api.Test;
 
 public class Ch10Test {
+    private int quantity;
+    private int itemPrice;
+
     private void sendAlert() {
         throw new UnsupportedOperationException();
     }
@@ -109,5 +112,23 @@ public class Ch10Test {
         }
 
         private TempRange _range;
+    }
+
+    public double getPrice() {
+        int basePrice = quantity * itemPrice;
+        int discountLevel;
+        if (quantity > 100)
+            discountLevel = 2;
+        else
+            discountLevel = 1;
+        double finalPrice = discountedPrice (basePrice, discountLevel);
+        return finalPrice;
+    }
+
+    private double discountedPrice (int basePrice, int discountLevel) {
+        if (discountLevel == 2)
+            return basePrice * 0.1;
+        else
+            return basePrice * 0.05;
     }
 }
