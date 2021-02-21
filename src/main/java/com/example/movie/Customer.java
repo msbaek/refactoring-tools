4f2255e76e4d432bde1292865841dbbcf6a10b65
msbaek <ms.baek@sk.com>
2021-02-21 20:04:19 +0900

    before Form Template Method

---
+++ src/main/java/com/example/movie/Customer.java
@@ -0,0 +1,60 @@
+package com.example.movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        String result = "Rental Record for " + getName() + "\n";
+        for (Rental each : rentals) {
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle()+ "\t" +
+                    String.valueOf(each.getCharge()) + "\n";
+        }
+
+        //add footer lines
+        result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+                " frequent renter points";
+        return result;
+    }
+
+    public String htmlStatement() {
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        for (Rental each : rentals) {
+            //show figures for each rental
+            result += each.getMovie().getTitle()+ ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        return 0;
+    }
+
+    private int getTotalCharge() {
+        return 0;
+    }
+}
+
---
+++ src/main/java/com/example/movie/Rental.java
@@ -0,0 +1,23 @@
+package com.example.movie;
+
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getCharge() {
+        return 0;
+    }
+}
