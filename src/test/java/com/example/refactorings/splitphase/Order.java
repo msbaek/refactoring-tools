5237c7f87cbbd49cd7d6d917ee262ff040c8af7d
Myeongseok Baek <codetemplate@hanmail.net>
2022-03-30 16:10:04 +0900

    4. extract first phase as method

--- src/test/java/com/example/refactorings/splitphase/Order.java
+++ src/test/java/com/example/refactorings/splitphase/Order.java
@@ -11,12 +11,17 @@ record PriceData(double basePrice, int quantity, double discount) {
 
 public class Order {
     double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        PriceData priceData = calculatePricingData(product, quantity);
+        double price = applyShippingPrice(priceData, shippingMethod);
+        return price;
+    }
+
+    private PriceData calculatePricingData(Product product, int quantity) {
         double basePrice = product.basePrice() * quantity;
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
         PriceData priceData = new PriceData(basePrice, quantity, discount);
-        double price = applyShippingPrice(priceData, shippingMethod);
-        return price;
+        return priceData;
     }
 
     private double applyShippingPrice(PriceData priceData, ShippingMethod shippingMethod) {
