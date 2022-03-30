3a5201ff1b8d13615c13c8b32cd9ac5a496da493
Myeongseok Baek <codetemplate@hanmail.net>
2022-03-30 16:02:19 +0900

    2. Introduce an intermediate data structure(PriceData)

--- src/test/java/com/example/refactorings/splitphase/Order.java
+++ src/test/java/com/example/refactorings/splitphase/Order.java
@@ -6,16 +6,20 @@ record Product(double basePrice, double discountThreshold, double discountRate) 
 record ShippingMethod(double discountThreshold, double discountedFee, Double feePerCase) {
 }
 
+record PriceData() {
+}
+
 public class Order {
     double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
         double basePrice = product.basePrice() * quantity;
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        double price = applyShippingPrice(quantity, shippingMethod, basePrice, discount);
+        PriceData priceData = new PriceData();
+        double price = applyShippingPrice(priceData, quantity, shippingMethod, basePrice, discount);
         return price;
     }
 
-    private double applyShippingPrice(int quantity, ShippingMethod shippingMethod, double basePrice, double discount) {
+    private double applyShippingPrice(PriceData priceData, int quantity, ShippingMethod shippingMethod, double basePrice, double discount) {
         double shippingPerCase = (basePrice > shippingMethod.discountThreshold())
                 ? shippingMethod.discountedFee() : shippingMethod.feePerCase();
         double shippingCost = quantity * shippingPerCase;
