6b2881230d061f72f3bd46afcafaab53a291c345
Myeongseok Baek <codetemplate@hanmail.net>
2022-03-30 17:01:10 +0900

    extract method - run
    - 2가지 일을 하는 함수
      - 주문 목록을 읽어서 개수를 세고,
      - 명령줄 인수를 담은 배열을 읽어서 프로그램의 동작을 결정
    - 첫번째 단계: 명령줄 인수의 구문을 분석해서 의미를 추출
    - 두번째 단계: 추출된 정보를 이용해서 데이터를 적절히 가공
    - 명령줄 프로그램은 테스트하기 어렵다. 매번 jvm을 구동해야하므로
      - 핵심 작업을 수행하는 코드 전부를 함수로 추출

--- src/test/java/com/example/refactorings/splitphase/CountOrder.java
+++ src/test/java/com/example/refactorings/splitphase/CountOrder.java
@@ -3,6 +3,7 @@ package com.example.refactorings.splitphase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
@@ -12,22 +13,26 @@ public class CountOrder {
 
     public static void main(String[] args) {
         try {
-            if (args.length == 0)
-                throw new RuntimeException("파일명을 입력하세요");
-            String filename = args[args.length - 1];
-            File input = Paths.get(filename).toFile();
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(input, Order[].class);
-            if (Arrays.asList(args).contains("-r")) {
-                System.out.println(Arrays.stream(orders)
-                                         .filter(o -> "ready".equals(o.status()))
-                                         .count());
-            }
-            else
-                System.out.println(orders.length);
+            run(args);
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
+    }
+
+    private static void run(String[] args) throws IOException {
+        if (args.length == 0)
+            throw new RuntimeException("파일명을 입력하세요");
+        String filename = args[args.length - 1];
+        File input = Paths.get(filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+        if (Arrays.asList(args).contains("-r")) {
+            System.out.println(Arrays.stream(orders)
+                                     .filter(o -> "ready".equals(o.status()))
+                                     .count());
+        }
+        else
+            System.out.println(orders.length);
     }
 }
