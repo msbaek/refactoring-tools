41a626d0e7b6dbd21f58edaf35f48bf75ee8a050
Myeongseok Baek <codetemplate@hanmail.net>
2022-03-30 16:07:11 +0900

    3.1.1 remove unused argument - basePrice

--- src/test/java/com/example/refactorings/splitphase/Order.java
+++ src/test/java/com/example/refactorings/splitphase/Order.java
@@ -15,11 +15,11 @@ public class Order {
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
         PriceData priceData = new PriceData(basePrice);
-        double price = applyShippingPrice(priceData, quantity, shippingMethod, basePrice, discount);
+        double price = applyShippingPrice(priceData, quantity, shippingMethod, discount);
         return price;
     }
 
-    private double applyShippingPrice(PriceData priceData, int quantity, ShippingMethod shippingMethod, double basePrice, double discount) {
+    private double applyShippingPrice(PriceData priceData, int quantity, ShippingMethod shippingMethod, double discount) {
         double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold())
                 ? shippingMethod.discountedFee() : shippingMethod.feePerCase();
         double shippingCost = quantity * shippingPerCase;
