34445259296d1a5f09041166a07f43f2454f3cbc
Myeongseok Baek <codetemplate@hanmail.net>
2022-04-01 15:55:41 +0900

    1/2 of applying functional core
    - replace out of process dependencies with  mockable interface

--- src/test/java/com/example/refactorings/splitphase/AuditManager.java
+++ src/test/java/com/example/refactorings/splitphase/AuditManager.java
@@ -4,12 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-class Directory {
-    public static String[] getFiles(String directoryName) {
-        throw new IllegalStateException("Directory#getFiles is not implemented yet");
-    }
-}
+import java.util.concurrent.atomic.AtomicInteger;
 
 class Sorted {
     public final int index;
@@ -21,64 +16,62 @@ class Sorted {
     }
 }
 
-class File {
-    public static void writeAllText(String newFile, String newRecord) {
-        throw new IllegalStateException("File#writeAllText is not implemented yet");
-    }
-
-    public static String [] readAllLines(String currentFilePath) {
-        throw new IllegalStateException("File#readAllLines is not implemented yet");
-    }
-}
-
 class Path {
     public static String combine(String directoryName, String fileName) {
         return directoryName + java.io.File.pathSeparator + fileName;
     }
 }
 
+interface FileSystem {
+    String[] getFiles(String directoryName);
+    void writeAllText(String filePath, String content);
+    List<String> readAllLines(String filePath);
+}
+
 public class AuditManager {
     private final int maxEntriesPerFile;
     private final String directoryName;
+    private final FileSystem fileSystem;
 
-    public AuditManager(int maxEntriesPerFile, String directoryName) {
+    public AuditManager(int maxEntriesPerFile, String directoryName, FileSystem fileSystem) {
         this.maxEntriesPerFile = maxEntriesPerFile;
         this.directoryName = directoryName;
+        this.fileSystem = fileSystem;
     }
 
     public void addRecord(String visitorName, LocalDateTime timeOfVisit) {
-        String[] filePaths = Directory.getFiles(directoryName);
+        String[] filePaths = fileSystem.getFiles(directoryName);
         Sorted[] sorted = sortByIndex(filePaths);
 
         String newRecord = visitorName + ';' + timeOfVisit;
 
         if (sorted.length == 0) {
             String newFile = Path.combine(directoryName, "audit_1.txt");
-            File.writeAllText(newFile, newRecord);
+            fileSystem.writeAllText(newFile, newRecord);
             return;
         }
 
         int currentFileIndex = sorted[sorted.length - 1].index;
         String currentFilePath = sorted[sorted.length - 1].path;
-        List<String> lines = new ArrayList<>(Arrays.asList(File.readAllLines(currentFilePath)));
+        List<String> lines = new ArrayList<>(fileSystem.readAllLines(currentFilePath));
 
         if (lines.size() < maxEntriesPerFile) {
             lines.add(newRecord);
             String newContent = String.join("\n", lines);
-            File.writeAllText(currentFilePath, newContent);
+            fileSystem.writeAllText(currentFilePath, newContent);
         }
         else {
             int newIndex = currentFileIndex + 1;
             String newName = "audit_" + newIndex + ".txt";
             String newFile = Path.combine(directoryName, newName);
-            File.writeAllText(newFile, newRecord);
+            fileSystem.writeAllText(newFile, newRecord);
         }
     }
 
     private Sorted[] sortByIndex(String[] filePaths) {
-        final Integer[] idx = {0};
+        AtomicInteger idx = new AtomicInteger(1);
         return Arrays.stream(filePaths)
-                     .map(s -> new Sorted(idx[0]++, s))
+                     .map(s -> new Sorted(idx.getAndIncrement(), s))
                      .toArray(Sorted[]::new);
     }
 }
--- src/test/java/com/example/refactorings/splitphase/AuditManagerTest.java
+++ src/test/java/com/example/refactorings/splitphase/AuditManagerTest.java
@@ -1,17 +1,47 @@
 package com.example.refactorings.splitphase;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AuditManagerTest {
+    @Mock
+    private FileSystem fileSystem;
+
     @Test
-    void addRecord() {
-        int maxAuditEntries = 0;
-        String directoryName = "~/Downloads/";
-        AuditManager auditManager = new AuditManager(maxAuditEntries, directoryName);
-        String visitorName = "visitorName";
-        LocalDateTime timeOfVisit = LocalDateTime.of(2022, 3, 25, 14, 0);
-        auditManager.addRecord(visitorName, timeOfVisit);
+    void addRecord_with_normal_case() {
+        String directoryName = "audits";
+        String[] files = {
+                directoryName + "\\audit_1.txt",
+                directoryName + "\\audit_2.txt"
+        };
+        List<String> lines = Arrays.asList(
+                "Peter; 2019-04-06T16:30:00",
+                "Jane; 2019-04-06T16:40:00",
+                "Jack; 2019-04-06T17:00:00"
+        );
+        LocalDateTime dateTime = LocalDateTime.of(2022, 3, 25, 14, 0);
+        String visitorName = "Alice";
+
+        when(fileSystem.getFiles(directoryName))
+                .thenReturn(files);
+        when(fileSystem.readAllLines(directoryName + "\\" + "audit_2.txt"))
+                .thenReturn(lines);
+
+        AuditManager auditManager = new AuditManager(3, directoryName, fileSystem);
+        auditManager.addRecord(visitorName, dateTime);
+
+        verify(fileSystem).writeAllText(
+                directoryName + ":audit_" + (files.length + 1) + ".txt",
+                visitorName + ";" + dateTime);
     }
 }
